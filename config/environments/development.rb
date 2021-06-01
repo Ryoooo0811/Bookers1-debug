3a660a8c6247108482ea2cac72a0f2ec81986c1c
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-06-01 08:55:32 +0000

    test finish

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
+  config.hosts << "d8fe066aa7764e54bc55f1c32e6a038e.vfs.cloud9.us-east-1.amazonaws.com"
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
